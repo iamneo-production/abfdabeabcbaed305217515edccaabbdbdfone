d239f6ea418e21665b7ea9ab234d7ad57ea9ac31
Iamneo <techadmin@iamneo.ai>
2023-09-18 11:10:41 +0000

    Backup Commit

--- reactapp/src/quiz/Quiz.jsx
+++ reactapp/src/quiz/Quiz.jsx
@@ -3,7 +3,65 @@ import React, { useState } from "react";
 import "./Quiz.css"; // Import your CSS file
 
 function App() {
-  
+  const [questions, setQuestions] = useState([
+    {
+      question: "What is the capital of France?",
+      options: ["London", "Berlin", "Paris", "Madrid"],
+      correctAnswer: 2,
+      userAnswer: null,
+    },
+    {
+      question: "Which planet is known as the Red Planet?",
+      options: ["Mars", "Jupiter", "Venus", "Mercury"],
+      correctAnswer: 0,
+      userAnswer: null,
+    },
+    {
+      question: "Who painted the Mona Lisa?",
+      options: [
+        "Vincent van Gogh",
+        "Pablo Picasso",
+        "Leonardo da Vinci",
+        "Michelangelo",
+      ],
+      correctAnswer: 2,
+      userAnswer: null,
+    },
+    {
+      question: "What is the largest mammal?",
+      options: ["African Elephant", "Blue Whale", "Giraffe", "Hippopotamus"],
+      correctAnswer: 1,
+      userAnswer: null,
+    },
+    {
+      question: "Which gas do plants use for photosynthesis?",
+      options: ["Oxygen", "Carbon Dioxide", "Nitrogen", "Hydrogen"],
+      correctAnswer: 1,
+      userAnswer: null,
+    },
+  ]);
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const [score, setScore] = useState(0);
+  const [quizCompleted, setQuizCompleted] = useState(false);
+
+  const handleAnswerSelect = (selectedOption) => {
+    if (!quizCompleted) {
+      const updatedQuestions = [...questions];
+      updatedQuestions[currentQuestionIndex].userAnswer = selectedOption;
+      setQuestions(updatedQuestions);
+
+      if (selectedOption === questions[currentQuestionIndex].correctAnswer) {
+        setScore(score + 1);
+      }
+
+      if (currentQuestionIndex === questions.length - 1) {
+        setQuizCompleted(true);
+      } else {
+        setCurrentQuestionIndex(currentQuestionIndex + 1);
+      }
+    }
+  };
+
   return (
     <div className="app-container">
       <h1>Quiz</h1>
